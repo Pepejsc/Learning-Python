eb82b893a12f47941ce2b9abc13d87e90d01d333
Edison José Sánchez Carrillo <70412564+Pepejsc@users.noreply.github.com>
2026-03-11 23:15:37 +0000

    new exercise for the project of python.

---
+++ Proyectouno.py
@@ -0,0 +1,7 @@
+#Madlib
+adjetivo = input("Escribe un adjetivo: ")
+verbo_uno = input("Escribe un verbo: ")
+verbo_dos = input("Escribe otro verbo: ")
+heroe = input("Escribe el nombre de un héroe: ")
+
+print(f"La programación es tan {adjetivo}, porque em permite {verbo_uno} mis ideas en código, y luego {verbo_dos} en una pantalla. ¡Es como ser un {heroe} de la tecnología!")
